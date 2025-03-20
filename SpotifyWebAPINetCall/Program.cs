57f83b9343028955def8a6ddba1d6c0772485d92
Muwaffaq Elbadawi <kingmoaffag10@gmail.com>
2025-03-21 00:56:22 +0300

    latest version

---
+++ SpotifyWebAPINetCall/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using SpotifyAPI.Web;
+
+class Program
+{
+  static async Task Main()
+  {
+    var spotify = new SpotifyClient("YourAccessToken");
+
+    var track = await spotify.Tracks.Get("1s6ux0lNiTziSrd7iUAADH");
+    Console.WriteLine(track.Name);
+  }
+}
